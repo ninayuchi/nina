d463679684f58b77c3f77a7aacd11d6641b680d1
Nina Y <nyuchi@risd.edu>
2018-06-06 21:17:14 -0400

    :)

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".image").draggable();
+ });
+ $(function() {
+    $(".ui-widget-content" ).resizable({
+      aspectRatio: 1/1
+    });
+  });
+
+$(document).ready(function(){
+	$("this").addClass("front");
+});
